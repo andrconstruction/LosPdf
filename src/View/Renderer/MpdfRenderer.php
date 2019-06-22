20738b6581d09109c0073845f94b6a4112e2f6c5
andrconstruction <andrace@yandex.ru>
2019-06-22 16:35:03 +0300

    Added font options

--- src/View/Renderer/MpdfRenderer.php
+++ src/View/Renderer/MpdfRenderer.php
@@ -1,66 +1,89 @@
 <?php
-/**
- * Mpdf Renderer file
- *
- * @author     Leandro Silva <leandro@leandrosilva.info>
- * @category   LosPdf
- * @license    https://github.com/Lansoweb/LosPdf/blob/master/LICENSE BSD-3 License
- * @link       http://github.com/LansoWeb/LosPdf
- */
-namespace LosPdf\View\Renderer;
+	/**
+	 * Mpdf Renderer file
+	 *
+	 * @author     Leandro Silva <leandro@leandrosilva.info>
+	 * @category   LosPdf
+	 * @license    https://github.com/Lansoweb/LosPdf/blob/master/LICENSE BSD-3 License
+	 * @link       http://github.com/LansoWeb/LosPdf
+	 */
 
-use mPDF;
-use LosPdf\View\Model\PdfModel;
+	namespace LosPdf\View\Renderer;
 
-/**
- * Mpdf Renderer class
- *
- * @author     Leandro Silva <leandro@leandrosilva.info>
- * @category   LosPdf
- * @license    https://github.com/Lansoweb/LosPdf/blob/master/LICENSE BSD-3 License
- * @link       http://github.com/LansoWeb/LosPdf
- */
-final class MpdfRenderer extends AbstractRenderer
-{
-    public function getEngine()
-    {
-        if ($this->engine === null) {
-            $this->engine = new \Mpdf\Mpdf();
-        }
+	use LosPdf\View\Model\PdfModel;
 
-        return $this->engine;
-    }
+	/**
+	 * Mpdf Renderer class
+	 *
+	 * @author     Leandro Silva <leandro@leandrosilva.info>
+	 * @category   LosPdf
+	 * @license    https://github.com/Lansoweb/LosPdf/blob/master/LICENSE BSD-3 License
+	 * @link       http://github.com/LansoWeb/LosPdf
+	 */
+	final class MpdfRenderer extends AbstractRenderer {
 
-    protected function doRender()
-    {
-        return $this->getEngine()->Output();
-    }
+		public function getEngine( $options = [] ) {
+			if ( $this->engine === null ) {
+				$this->engine = new \Mpdf\Mpdf( $options );
+			}
 
-    protected function doPrepare($model, $values)
-    {
-        $this->html = $this->renderer->render($model, $values);
 
-        $paperOrientation = $this->getOption(PdfModel::PAPER_ORIENTATION, PdfModel::ORIENTATION_PORTRAIT);
-        $paperSize = $this->getOption(PdfModel::PAPER_SIZE, PdfModel::SIZE_A4);
+			return $this->engine;
+		}
 
-        if (!is_array($paperSize)) {
-            $format = strtolower($paperOrientation[0]);
-            if ($format == 'l') {
-                $paperSize = $paperSize.'-'.$format;
-            }
-        }
+		protected function doRender() {
+			return $this->getEngine()->Output();
+		}
 
-        $this->getEngine()->_setPageSize($paperSize, $paperOrientation);
-        $this->getEngine()->WriteHTML($this->html);
-    }
+		protected function doPrepare( $model, $values ) {
+			$options = [];
 
-    protected function doRenderToString(PdfModel $model)
-    {
-        return $this->getEngine()->Output('', 'S');
-    }
+			$this->html = $this->renderer->render( $model, $values );
 
-    protected function doRenderToFile(PdfModel $model, $fileName)
-    {
-        return $this->getEngine()->Output($fileName, 'F');
-    }
-}
+			$paperOrientation = $this->getOption( PdfModel::PAPER_ORIENTATION, PdfModel::ORIENTATION_PORTRAIT );
+			$paperSize        = $this->getOption( PdfModel::PAPER_SIZE, PdfModel::SIZE_A4 );
+
+			if ( !is_array( $paperSize ) ) {
+				$format = strtolower( $paperOrientation[ 0 ] );
+				if ( $format == 'l' ) {
+					$paperSize = $paperSize . '-' . $format;
+				}
+			}
+
+			if ( $this->getOption( PdfModel::FONT_EXT, [] ) ) {
+
+				$opt = $this->getOption( PdfModel::FONT_EXT, [] );
+
+				if ( isset( $opt[ 'fontDir' ] ) ) {
+					$defaultConfig = ( new \Mpdf\Config\ConfigVariables() )->getDefaults();
+					$fontDirs      = $defaultConfig[ 'fontDir' ];
+
+					$options[ 'fontDir' ] = array_merge( $fontDirs, $opt[ 'fontDir' ] );
+				}
+
+				if ( isset( $opt[ 'fontdata' ] ) ) {
+					$defaultFontConfig     = ( new \Mpdf\Config\FontVariables() )->getDefaults();
+					$fontData              = $defaultFontConfig[ 'fontdata' ];
+					$options[ 'fontdata' ] = $fontData + $opt[ 'fontdata' ];
+				}
+
+				if ( isset( $opt[ 'default_font' ] ) ) {
+					$options[ 'default_font' ] = $opt[ 'default_font' ];
+				}
+
+			}
+
+			$this->getEngine( $options );
+
+			$this->getEngine()->_setPageSize( $paperSize, $paperOrientation );
+			$this->getEngine()->WriteHTML( $this->html );
+		}
+
+		protected function doRenderToString( PdfModel $model ) {
+			return $this->getEngine()->Output( '', 'S' );
+		}
+
+		protected function doRenderToFile( PdfModel $model, $fileName ) {
+			return $this->getEngine()->Output( $fileName, 'F' );
+		}
+	}
